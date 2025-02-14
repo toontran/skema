3ee56a7863c3595b287c3efb141bbca0bfae83b4
Toon Tran <sontungtran99@gmail.com>
2025-02-13 21:17:00 -0500

    run script

---
+++ run_script.sh
@@ -0,0 +1,2 @@
+#./build/skema --m 27 --n 27 --input test/data/symmetric_dense.txt --kernel gaussrbf --print-level 3 --debug-level 3 --save-S
+./build/skema --m 27 --n 27 --input test/data/symmetric_dense.txt --kernel gaussrbf --print-level 3 --debug-level 3 --save-S --solver isvd
