c3fb7a308190a34e6a68b45e5bb211f9058126bf
Toon Tran <sontungtran99@gmail.com>
2025-02-14 08:28:36 -0500

    build_script

---
+++ build_script.sh
@@ -0,0 +1,31 @@
+git submodule update --init --recursive
+
+# 1. Build Kokkos
+cd ~/pj/skema/kokkos
+mkdir -p build
+cd build
+cmake -G Ninja -DCMAKE_INSTALL_PREFIX=./install ..
+ninja
+ninja install
+
+# 2. Build KokkosKernels
+cd ~/pj/skema/kokkos-kernels
+mkdir -p build
+cd build
+KOKKOS_PATH=$(readlink -f ../../kokkos/build/install)
+cmake -G Ninja \
+  -DKokkos_DIR=${KOKKOS_PATH}/lib/cmake/Kokkos \
+  -DCMAKE_INSTALL_PREFIX=./install \
+  ..
+ninja
+ninja install
+
+# 3. Build main project (skema)
+cd ~/pj/skema
+mkdir -p build
+cd build
+KERNELS_PATH=$(readlink -f ../kokkos-kernels/build/install)
+cmake -G Ninja \
+  -DKokkosKernels_DIR=${KERNELS_PATH}/lib/cmake/KokkosKernels \
+  ..
+ninja
