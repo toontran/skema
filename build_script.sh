33932cdc0b75c55ed619124442dac8d350a0f0cc
toontran <sontungtran99@gmail.com>
2025-02-18 08:50:38 -0500

    scripts

--- build_script.sh
+++ build_script.sh
@@ -1,24 +1,25 @@
-git submodule update --init --recursive
 
-# 1. Build Kokkos
-cd ~/pj/skema/kokkos
-mkdir -p build
-cd build
-cmake -G Ninja -DCMAKE_INSTALL_PREFIX=./install ..
-ninja
-ninja install
+git submodule update --init --recursive # This did all the below
 
-# 2. Build KokkosKernels
-cd ~/pj/skema/kokkos-kernels
-mkdir -p build
-cd build
-KOKKOS_PATH=$(readlink -f ../../kokkos/build/install)
-cmake -G Ninja \
-  -DKokkos_DIR=${KOKKOS_PATH}/lib/cmake/Kokkos \
-  -DCMAKE_INSTALL_PREFIX=./install \
-  ..
-ninja
-ninja install
+# # 1. Build Kokkos
+# cd ~/pj/skema/kokkos
+# mkdir -p build
+# cd build
+# cmake -G Ninja -DCMAKE_INSTALL_PREFIX=./install ..
+# ninja
+# ninja install
+
+# # 2. Build KokkosKernels
+# cd ~/pj/skema/kokkos-kernels
+# mkdir -p build
+# cd build
+# KOKKOS_PATH=$(readlink -f ../../kokkos/build/install)
+# cmake -G Ninja \
+#   -DKokkos_DIR=${KOKKOS_PATH}/lib/cmake/Kokkos \
+#   -DCMAKE_INSTALL_PREFIX=./install \
+#   ..
+# ninja
+# ninja install
 
 # 3. Build main project (skema)
 cd ~/pj/skema
@@ -29,3 +30,4 @@ cmake -G Ninja \
   -DKokkosKernels_DIR=${KERNELS_PATH}/lib/cmake/KokkosKernels \
   ..
 ninja
+
--- run_script.sh
+++ run_script.sh
@@ -1,1 +1,1 @@
-./bin/skema --input ../test/data/matrix_data_kernel_random_30_2.0.mtx --symmetric --rank 10 --window 10 --method isvd --print-level 3
+./build/bin/skema --input ./test/data/matrix_data_kernel_random_30_2.0.mtx --symmetric --rank 10 --window 10 --method isvd --print-level 3
