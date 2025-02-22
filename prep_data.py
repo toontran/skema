257bb3480fd49d35fe4c2e14d146b72d5a730001
toontran <sontungtran99@gmail.com>
2025-02-22 13:20:27 -0500

    prep data

--- prep_data.py
+++ prep_data.py
@@ -5,10 +5,12 @@ home_dir = os.path.expanduser("~")
 filepath = os.path.join(home_dir, f'data/data_2m.mtx')
 A = scipy.io.mmread(filepath)
 
-print(type(A))
-import pdb;pdb.set_trace()
+# print(type(A))
+# import pdb;pdb.set_trace()
 
-# scipy.io.mmwrite(filename, A_csr)
+length = 1000
+target_path =f'./test/data/data_{length}.mtx'
+scipy.io.mmwrite(target_path, A[:length, :])
 # print(f"Matrix saved in", filename)
 # if len(S_exact) < 100:
 #     print(S_exact)
