9be13346d09deb5b92c84eb3e29de8a6b6cc9eee
toontran <sontungtran99@gmail.com>
2025-03-02 09:48:36 -0500

    debug

--- src/Skema_ISVD_Primme.cpp
+++ src/Skema_ISVD_Primme.cpp
@@ -29,7 +29,7 @@ void ISVD_SVDS<MatrixType>::compute(const MatrixType& X,
   vector_type svecs("svecs", (nrow + ncol) * rank);
   vector_type rnrms("rnrms", rank);
 
-  printf("nrow: %zu, ncol: %zu, rank: %zu\n", nrow, ncol, rank);
+  // printf("nrow: %zu, ncol: %zu, rank: %zu\n", nrow, ncol, rank);
 
   /* Set primme_svds parameters */
   using primme_svds = PRIMME_SVDS<MatrixType>;
